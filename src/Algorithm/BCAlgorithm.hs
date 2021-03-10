cd69a7b7d943ccaf37ccf6349cf277b3c456abb4
holmusk-mac1 <holmusk-mac1@holmusk-mac1s-MacBook-Pro.local>
2021-03-10 15:28:36 +0800

    bug fixed and more test needed

--- src/Algorithm/BCAlgorithm.hs
+++ src/Algorithm/BCAlgorithm.hs
@@ -35,6 +35,15 @@ type PathRecords = [PathRecord]
 
 data Board = Board {lucky :: SearchRecords , waiting :: PathRecords, futile :: SearchRecords, seen :: D.Language} deriving(Show)
 
+query:: 
+    ( Has D.Language env  
+    , UseRuleOnly env 
+    , MonadReader env m
+    , MonadIO m 
+    , OrderingContext env
+    ) => D.Argument ->  m Defeater 
+query incArgu = queryArgument incArgu []
+
 -- | rewrite query 
 queryArgument :: 
     ( Has D.Language env  
@@ -42,10 +51,11 @@ queryArgument ::
     , MonadReader env m
     , MonadIO m 
     , OrderingContext env
-    ) => D.Argument -> m Defeater 
-queryArgument incArgu = do 
+    ) => D.Argument -> D.Language ->  m Defeater 
+queryArgument incArgu accSeen = do 
     let 
-        initBoard = initialBoard incArgu 
+        tmpBoard = initialBoard incArgu 
+        initBoard = tmpBoard{seen=accSeen}
     defeatChain initBoard 
 
 -- | rewrite of chain construction 
@@ -58,6 +68,7 @@ defeatChain ::
     ) => Board -> m Defeater
 defeatChain step1Board = do 
     step1 <- defeatDetection step1Board 
+    liftIO $ print step1
     case step1 of 
         Right p -> pure $ warranted p 
         Left step2Board -> do 
@@ -120,8 +131,10 @@ defeatDetection ::
     ) => Board -> m (Either Board SearchRecord)
 defeatDetection board@Board{..} = do 
         (newWaiting, newSeen, newLucky) <- checkLuckySet seen lucky
-        if checkLuckyComplete newLucky 
-            then pure $ Right $ selectOneLucker newLucky 
+        let 
+            completeLuckers = checkLuckyComplete newLucky 
+        if (not . null) completeLuckers 
+            then pure $ Right $ selectOneLucker completeLuckers 
             else pure $ Left $ board{lucky=newLucky, waiting=waiting ++newWaiting, seen =newSeen}
 
 {- 3
@@ -218,7 +231,7 @@ defeaterChecker board@Board{..} =
         [] -> pure $ Right $ unwarranted futile 
         _ -> do 
             ((p,incArgument), tmpWaiting) <- selectionTwo waiting 
-            result <- queryArgument incArgument
+            result <- queryArgument incArgument seen 
             case tellQuery result of 
                 Warranted ->  do
                     let 
@@ -344,16 +357,20 @@ checkConflict ::
     , UseRuleOnly env 
     , OrderingContext env 
     ) => D.Path -> Conflict -> m D.Language 
-checkConflict p (Undercut l)= pure [l]
-checkConflict p Peace = pure [] 
+checkConflict _ (Undercut l)= pure [l]
+checkConflict _ Peace = pure [] 
 checkConflict p (Rebut l) = do 
     let 
         defP = branchDef p [M.neg l]
-    necPaths <- getNecPath l 
-    rs <- mapM (checkDefeat defP) necPaths 
-    if or rs 
-        then pure [l] 
-        else pure []  
+    necPaths <- getNecPath l
+    if 
+        null defP 
+        then pure []
+        else  do 
+            rs <- mapM (checkDefeat defP) necPaths 
+            if or rs 
+                then pure [l] 
+                else pure []  
 
 
 createPathRecord ::
@@ -509,21 +526,12 @@ initAgu::
 initAgu ls = do
     subLevel <- mapM concludeBy ls 
     pure [[p] | p <- foldr createParallel [[]] subLevel]
-    where 
-        createParallel :: [a] -> [[a]] -> [[a]] 
-        createParallel paths ls = do 
-                pa <- paths
-                a <- ls 
-                pure $  pa:a  
 
-agu :: 
-    ( MonadReader env m
-    , UseRuleOnly env 
-    , MonadIO m 
-    ) => D.Argument  -> m D.Argument 
-agu argument = do 
-        arguments <- mapM pathExtend argument 
-        pure $ concat arguments 
+createParallel :: [a] -> [[a]] -> [[a]] 
+createParallel paths ls = do 
+        pa <- paths
+        a <- ls 
+        pure $  pa:a  
 
 aguFixpoint :: 
     ( MonadReader env m
@@ -542,7 +550,15 @@ augDefeasible ::
     , MonadIO m 
     ) => D.Argument  -> m D.Argument 
 augDefeasible = aguFixpoint 
-    
+
+agu :: 
+    ( MonadReader env m
+    , UseRuleOnly env 
+    , MonadIO m 
+    ) => D.Argument  -> m D.Argument 
+agu argument = do 
+        arguments <- mapM pathExtend argument 
+        pure $ concat arguments 
 
 pathExtend :: 
     ( MonadReader env m
@@ -560,17 +576,12 @@ pathExtend path = do
         do 
     supportRules <- mapM concludeBy bodies 
     let 
-        parallelPathSection = foldr parallel [[]] supportRules
+        parallelPathSection = foldr createParallel [[]] supportRules
         newArgument = do 
                 pathSection <- parallelPathSection
                 pure $ path ++ [pathSection] 
     pure newArgument
-    where 
-        parallel :: [a] -> [[a]] -> [[a]] 
-        parallel paths ls = do 
-                pa <- paths
-                a <- ls 
-                pure $  pa:a 
+        
 
 concludeBy :: 
         ( MonadReader env m
@@ -584,8 +595,8 @@ concludeBy l = do
         globalRules = D.getDefeasibleRules dRules ++ D.getStrictRules sRules
     pure [r | r<- globalRules, D.conC r == l ]
 
-checkLuckyComplete :: SearchRecords -> Bool 
-checkLuckyComplete rs = null [r | r <- rs, reachGround (fst r) ]
+checkLuckyComplete :: SearchRecords -> SearchRecords
+checkLuckyComplete rs = [r | r <- rs, reachGround (fst r) ]
 
 reachGround :: D.Path -> Bool 
 reachGround path = 
@@ -594,235 +605,3 @@ reachGround path =
     in case concat (D.body <$> i) of 
         [] -> True 
         _ -> False 
-
-{-Legacy Code-}
-
--- {-step 0 -}
--- query ::D.PreferenceMap -> D.Language -> D.Argument -> Defeater 
--- query pm lang incArgu = 
---     let 
---         initBoard = initialBoard incArgu 
---     in chainConstruction pm lang initBoard 
--- 
--- chainConstruction ::D.PreferenceMap -> D.Language -> Board -> Defeater 
--- chainConstruction pm lang step1Board = 
---     case defeatCheck pm lang step1Board of                          -- step 2 : check defeat  : update 'lucky' & 'waiting'
---         Right p -> warranted p                                  -- step 5: return warranted defeater  
---         Left step2Board -> 
---             case pathSelection' step2Board of                -- step 3: path selection  : update 'lucky' , set 'base'
---                 Just (base,step3Board) -> 
---                     let 
---                         luckyExtend= augConstruction lang base   -- step 4: new lucky set 
---                         step3Lucky = lucky step3Board
---                         step4Board = step3Board{lucky=step3Lucky++luckyExtend}
---                     in chainConstruction pm lang step4Board
---                 Nothing -> 
---                     case luckyEmpty pm lang step2Board of           -- step 6: update 'waiting' for sure , update 'lucky' (step 9) or 'futile' (step 7)
---                         Right unw -> unw                        -- step 8
---                         Left step6Board -> chainConstruction pm lang step6Board
--- 
--- {- 2
--- - Given Board from step 1
---     - Check 'lucky': If any unseen inc-defeater found for each path: 
---         - include all newly defeated target to seen
---         - Attaches the inc-defeater to related Path , move this PathRecord to 'waiting'
---         - If path has old related defeater(much be unwarranted defeater), then remove this old defeater and attach this new in-defeater. 
---             - a SearchRecord turns to be a PathRecord
---     - Check if any SearchRecord in 'lucky' contains complete Path. 
---         - Yes: go to 5 
---             - Exit : Pass the complete Path Record to 5 and get return result 
---         - No : go to 3 
---             - Continue construction
--- Notes:
---     1. when detecting one search record in lucky has new in-defeater, remove old fail-defeater, then put to tmpWaiting
---         inc-Argument defeat path
---     2. select one lucker from possibly many in lucky
--- -}
--- defeatCheck :: D.PreferenceMap ->  D.Language -> Board -> Either Board SearchRecord
--- defeatCheck pm lang board@Board{..}= 
---     let 
---         (newWaiting, newSeen, newLucky) = defeaterFilter' pm lang seen lucky    -- note 1
---     in 
---         if checkLuckyComplete newLucky 
---             then 
---                 Right $ selectOneLucker newLucky                            -- note 2
---             else 
---                 Left $ board{lucky = newLucky,waiting = waiting ++ newWaiting, seen=newSeen}
--- 
--- -- | Check lucy set one by one
--- -- the one with defeater move to wait, and add defeat target to seen. 
--- -- the one with no defeater stays in lucy
--- defeaterFilter' :: D.PreferenceMap -> D.Language ->  D.Language -> SearchRecords-> (PathRecords, D.Language, SearchRecords)
--- defeaterFilter' pm _    seen [] = ([],seen,[])
--- defeaterFilter' pm lang seen (r:rs) = 
---     let 
---         (re, newSeen)  = testLucker' pm lang seen r 
---         (newwait, ss , nlucy) = defeaterFilter' pm lang newSeen rs 
---     in 
---         case re of 
---             Right sr -> (newwait,ss, sr : nlucy)
---             Left pr -> (pr : newwait, ss, nlucy)
---     where
---         testLucker' ::D.PreferenceMap -> D.Language -> D.Language -> SearchRecord -> (Either PathRecord SearchRecord, D.Language)
---         testLucker' pm  lang seen sr@(p,_) = 
---             let 
---                 rebutters = selectRebutters lang seen p 
---                 undercutters = selectUndercutters lang seen p 
---                 defeaters = selectDefeaters' pm lang p rebutters undercutters
---             in 
---                 if null defeaters 
---                     then (Right sr, seen)
---                     else 
---                         let 
---                             newSeen = updateSeen seen defeaters 
---                             newPathRecord = createPathRecord' lang p defeaters
---                         in (Left newPathRecord, newSeen )
---
--- luckyEmpty ::D.PreferenceMap -> D.Language -> Board -> Either Board Defeater 
--- luckyEmpty pm lang board@Board{..} = case waiting of 
---     [] -> Right $ unwarranted futile 
---     _ -> let 
---             ((p,incArgument), tmpWaiting) = selectionTwo' waiting 
---             result = query pm lang incArgument 
---          in case tellQuery result of 
---                 Warranted -> 
---                     let 
---                         newBoard = waitingToFutile (p,result) tmpWaiting board
---                     in  luckyEmpty pm lang newBoard
---                 Unwarranted -> 
---                     let 
---                         newBoard = survived (p,result) tmpWaiting board
---                     in  Right $ chainConstruction pm lang newBoard 
-
--- createPathRecord' :: D.Language -> D.Path ->  D.Language -> PathRecord
--- createPathRecord' lang p defeaters = (p, initIncArgument' lang defeaters)
-
--- -- | TODO: Rebuters winner path need to be preserved
--- selectDefeaters' ::D.PreferenceMap -> D.Language -> D.Path -> D.Language -> D.Language -> D.Language
--- selectDefeaters' _  _    _     [] uns@(x:_)= uns 
--- selectDefeaters' pm lang p   res uns = checkRebutters lang p res ++ uns
---     where 
---         checkRebutters :: D.Language -> D.Path -> D.Language -> D.Language 
---         checkRebutters _    _ [] = []
---         checkRebutters lang p (u:us) = 
---             case lastLinkChecker p [M.neg u] of 
---                 (False,_) -> checkRebutters lang p us 
---                 (True, defRules) -> 
---                     let 
---                         attackDefs = attackerLastScan lang u 
---                         ordPrem = ord pm 
---                     in 
---                         if any (`ordPrem` defRules) attackDefs 
---                         then u : checkRebutters lang p us 
---                         else checkRebutters lang p us 
-
--- -- | TODO: 构建 defeater 的 path 应该把有效的保留下来， 不同长度的也可以完美应对，因为其实每次的base都只有一个，所以无所谓的.
--- --  and this is acutally a great optimization point. 
--- attackerLastScan :: D.Language -> D.Literal -> [D.Language]
--- attackerLastScan lang l = 
---     let 
---         initArg = initIncArgument' lang [l]  
---     in  completeLastChecker lang initArg 
-
--- initIncArgument' :: D.Language -> D.Language -> D.Argument 
--- initIncArgument' lang props = 
---     let 
---         rulesSet = concludeBy' lang <$> props 
---     in  [[p] |p <- foldr createParallel [[]] rulesSet]
---     where 
---         createParallel :: [a] -> [[a]] -> [[a]] 
---         createParallel paths ls = do 
---                         pa <- paths
---                         a <- ls 
---                         pure $  pa:a         
-
-
--- concludeBy' :: D.Language -> D.Literal -> D.Language 
--- concludeBy' lang l = [r | r <- lang, D.conC r == l ]
-
--- agu' :: D.Language  -> D.Argument -> D.Argument 
--- agu' lang argument =
---     let  
---         arguments = pathExtend' lang <$> argument 
---     in concat arguments 
-
--- pathExtend' :: D.Language -> D.Path -> D.Argument 
--- pathExtend' lang path = 
---     let 
---         rules = last path 
---         bodies = concat $ D.body <$> rules 
---     in 
---         if null bodies 
---             then [path]
---             else 
---                 let  
---                     supportRules = concludeBy' lang <$> bodies 
---                     parallelPathSection = foldr parallel [[]] supportRules
---                     newArgument = do 
---                             pathSection <- parallelPathSection
---                             pure $ path ++ [pathSection] 
---                 in newArgument
---     where 
---         parallel :: [a] -> [[a]] -> [[a]] 
---         parallel paths ls = do 
---                 pa <- paths
---                 a <- ls 
---                 pure $  pa:a  
-
--- augConstruction :: D.Language -> SearchRecord -> SearchRecords
--- augConstruction lang (p,defeater) = 
---     let 
---         tmpArg = pathExtend' lang p 
---     in (,defeater) <$> tmpArg 
-
--- completeLastChecker :: D.Language -> D.Argument -> [D.Language]
--- completeLastChecker _   []        = [] 
--- completeLastChecker lang argument = 
---     let
---         a = do 
---             p <- argument 
---             let c = lastLinkChecker p lang 
---             guard $ fst c 
---             pure $ snd c 
---         b = do 
---             p <- argument 
---             let c = lastLinkChecker p lang
---             guard $ not (fst c) 
---             pure p 
---     in a ++ completeLastChecker lang (agu' lang b)
-
--- pathSelection' :: Board -> Maybe (SearchRecord, Board)
--- pathSelection' board@Board{..} = case lucky of 
---     [] -> Nothing 
---     _ -> Just (base, newBoard)
---     where 
---         (base, newlucky) = selectionOne' lucky
---         newBoard = board{lucky=newlucky}
--- 
--- selectionOne' :: SearchRecords -> (SearchRecord, SearchRecords)
--- selectionOne' srs =
---     let 
---         sortByLength = sortBy (flip compare `on` (length . fst)) srs 
---         sortByDefeasible = sortBy (flip compare `on` (length . getDef . fst)) srs 
---     in (head sortByDefeasible, tail sortByDefeasible)  
---     where 
---         getDef :: D.Path -> D.Language 
---         getDef p = 
---             let 
---                 rules = concat p 
---             in [r | r<-rules, D.imp r == M.D]
--- 
-
--- selectionTwo' :: PathRecords -> (PathRecord, PathRecords)
--- selectionTwo' prs = 
---     let 
---         sortByLength = sortBy (flip compare `on` (length . fst)) prs  
---         sortByDefeasible = sortBy (flip compare `on` (length . getDef . fst)) prs 
---     in (head sortByDefeasible, tail sortByDefeasible)  
---     where 
---         getDef :: D.Path -> D.Language 
---         getDef p = 
---             let 
---                 rules = concat p 
---             in [r | r<-rules, D.imp r == M.D]
---             
